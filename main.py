0b40d467cdd42a94f36f32af3978aebe4e6c9fba
xuezhengrui <58976266+xuezhengrui@users.noreply.github.com>
2022-02-08 00:15:30 +0800

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from homePage import *
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Fake-Image Detection Program 1.0')
+    homePage(root)
+    root.mainloop()
+
+# For testing you can use following URL:
+# https://weibo.com/2146226217/Ithnlkypn
+# https://weibo.com/2146226217/I10Rs06kk
